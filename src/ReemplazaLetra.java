373ade5c671b8760e045887dd40243d57a8892d5
merrash <merrash@gmail.com>
2014-10-28 08:56:30 +0100

    .

--- src/ReemplazaLetra.java
+++ src/ReemplazaLetra.java
@@ -8,7 +8,7 @@ public class ReemplazaLetra {
 
       System.out.printf( "s1 = %s\ns2 = %s\ns3 = %s\n\n", s1, s2, s3 );
 
-      // test method replace      
+      // probandolo     
       System.out.printf("Reemplaza 'l' por 'L' en s1: %s\n\n", s1.replace( 'l', 'L' ) );
       System.out.printf("Reemplaza 'D' por 'R' en s1: %s\n\n", s2.replace( 'D', 'R' ) );
       System.out.printf("Reemplaza espacio por '*' en s1: %s\n\n", s3.replace( ' ', '*' ) );
